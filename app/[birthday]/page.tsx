ba7ae72e3b24419cce41753cca2b6612406e8b7b
marwinburesch <marwin@posteo.de>
2025-04-15 21:19:23 +0200

    🚨 Remove unused import

--- app/[birthday]/page.tsx
+++ app/[birthday]/page.tsx
@@ -1,5 +1,4 @@
 import MilestoneCard from "@days-old/components/MilestoneCard";
-import daysPassed from "@days-old/lib/daysPassed";
 import DaysOld from "@days-old/components/DaysOld";
 
 export default async function Page({
