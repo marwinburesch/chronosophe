dcd5032aaf437523d0165b45a643025452affd18
marwinburesch <marwin@posteo.de>
2025-04-16 08:25:04 +0200

    ✨ Animate DaysOld number

--- components/DaysOld.tsx
+++ components/DaysOld.tsx
@@ -1,20 +1,26 @@
 "use client";
 
-import useFormattedNumber from "@days-old/hooks/useFormattedNumber";
 import daysPassed from "@days-old/lib/daysPassed";
+import AnimatedNumber from "./AnimatedNumber";
+import { useEffect, useState } from "react";
 
 type DaysOldProps = {
   date: string;
 };
 
 const DaysOld: React.FC<DaysOldProps> = ({ date }) => {
+const [days, setDays] = useState(0);
   const { rounded } = daysPassed(date, 1);
-  const formattedAge = useFormattedNumber(rounded.age);
+
+useEffect(() => {
+    setDays(rounded.age);
+  }
+, [rounded.age]);
 
   return (
     <>
       <div className="text-4xl py-6">You are</div>
-      <div className="text-8xl font-bold">{formattedAge}</div>
+      <div className="text-8xl font-bold"><AnimatedNumber value={days} /></div>
       <div className="text-4xl py-6">days old!</div>
     </>
   );
